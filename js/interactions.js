e32062c9d083325e244c060bcbbae72daeda97af
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2023-05-06 22:58:57 -0600

    scripts

--- js/interactions.js
+++ js/interactions.js
@@ -1,5 +1,3 @@
 import { replaceTheme } from './utils.js';
 
 document.querySelector('button#theme-button').addEventListener('click', replaceTheme);
-
-document.querySelector('select#lang').addEventListener()
