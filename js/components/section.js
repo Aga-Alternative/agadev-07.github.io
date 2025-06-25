ed881dc1dbaec636181a7677a7a38c07061e1be0
Adrian <adrian07pro1@gmail.com>
2025-06-25 11:00:18 -0600

    last-odd

--- js/components/section.js
+++ js/components/section.js
@@ -1,3 +1,5 @@
+const AGA_PROJECT_TAG = 'aga-project';
+
 export default class AgaSection extends HTMLElement {
   constructor() {
     super();
@@ -11,8 +13,15 @@ export default class AgaSection extends HTMLElement {
     $description.setAttribute('data-lang-key', `${attrName}_description`);
     const $container = document.createElement('div');
     $container.classList.add('container');
-    [...this.children].forEach($child=>$container.appendChild($child));
-    this.append($title);
+    [...this.children].forEach(($child, i, list) =>{
+      // Obtener el aga-project para poder aplicar la clase last 
+      const $agaProject = $child.tagName.toLowerCase() == AGA_PROJECT_TAG ? $child : $child.querySelector(AGA_PROJECT_TAG);
+      const isLast = (list.length - i) == 1;
+      const isOdd = i % 2 == 0;
+      if ($agaProject && isLast && isOdd) $agaProject.className = "last-odd";
+      $container.appendChild($child)
+    })
+    this.appendChild($title);
     this.appendChild($description);
     this.appendChild($container);
   }
